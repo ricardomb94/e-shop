d229d365cef1f00fd2d6e30b3bc6dcae3ae72b55
ricardomb94 <aymardmb@gmail.com>
2020-12-30 08:52:38 +0100

    Fetching products (from React useEffect Hooks) and add proxy to display them

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../products';
+// import products from '../products';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  //let define the state
+  const [products, setProducts] = useState([]);
+
+  /**
+   * Let's make a request to the backend to change the state as soon as
+   * the component monte: display products
+   */
+  useEffect(() => {
+    //let's fetch products from our route API
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
   return (
     <>
       <h1 className="text-center">Produits Récents</h1>
