c98552b28b8754e436dcbdec185534d96576834b
ricardomb94 <aymardmb@gmail.com>
2021-06-05 04:54:33 +0200

    Test-2: success

--- frontend/src/actions/usersActions.js
+++ frontend/src/actions/usersActions.js
@@ -10,6 +10,9 @@ import {
     USER_DETAILS_REQUEST, 
     USER_DETAILS_SUCCESS, 
     USER_DETAILS_FAIL ,
+    USER_UPDATE_PROFILE_REQUEST,
+    USER_UPDATE_PROFILE_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL,
     USER_UPDATE_PROFILE_RESET,
 } from '../constants/userConstants'
 
@@ -121,6 +124,46 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
     }catch(error){
         dispatch({
             type: USER_DETAILS_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message
+            :error.message,
+        })
+    }
+}
+
+/*
+* USER UPDATE PROFILE
+*/
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type:USER_UPDATE_PROFILE_REQUEST,
+        })
+
+        //That gives us access to login object
+        const {userLogin:{ userInfo}} = getState()
+
+        const config ={
+            headers:{
+                'Content-Type': 'application/json', 
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+        const {data} = await axios.put(`/api/users/profile`, user, config)
+        console.log(data)
+
+        dispatch({
+            type:USER_UPDATE_PROFILE_SUCCESS,
+            payload: data,
+        })
+
+        dispatch({
+            type:USER_UPDATE_PROFILE_RESET,
+            payload: data
+        })
+        localStorage.setItem('userInfo', JSON.stringify(data))
+    }catch(error){
+        dispatch({
+            type: USER_UPDATE_PROFILE_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message
             :error.message,
         })
