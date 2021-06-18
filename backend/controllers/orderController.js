5c83cac970cc309e178ea47a20f09c3bc3939440
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2021-06-18 19:33:53 +0200

    Fixed bug

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,40 @@
+import Order from "../models/orderModels.js";
+import asyncHandler from "express-async-handler";
+
+// @desc create new oredr
+// @route GET /api/orders
+// @ccess Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+    return;
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
+});
+
+export { addOrderItems };
