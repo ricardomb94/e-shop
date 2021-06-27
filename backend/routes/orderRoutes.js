1af1516679e3a7488c9e3abbb3f5d3cd5c956e3a
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2021-06-27 20:56:35 +0200

    Checkout Process: orderControllers order routes set up

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,16 @@
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid
+} from "../controllers/orderController.js";
+
 import express from "express";
-const router = express.Router();
-import { addOrderItems, getOrderById } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
 
 router.route("/").post(protect, addOrderItems);
 router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 
 export default router;
