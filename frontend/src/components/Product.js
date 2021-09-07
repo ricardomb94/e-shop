1aeb0314906e3b678a81a89e43d9dd0b6068cc1e
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2021-09-07 13:17:31 +0200

    Price correction

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -16,7 +16,7 @@ const Product = ({ product }) => {
           </Card.Title>
         </Link>
         <Card.Text as="h3" className="text-center">{product.price}$<Link to={`/product/${product._id}`}>
-          <Button variant="dark custom-blue rounded" className="shadow-lg ml-5">Order know</Button>
+          <Button variant="dark custom-blue rounded" className="shadow-lg ml-5">Order now</Button>
         </Link></Card.Text>
 
         <hr />
