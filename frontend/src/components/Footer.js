1de375027293c1e0cf66b80f23ea7a691f503c0d
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2021-09-09 01:04:23 +0200

    Set date in the footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className="text-center py-3">Copyright &copy; Kivani Clothing {Date.now()}</Col>
+          <Col className="text-center py-3">Copyright &copy; Kivani Clothing {new Date().getFullYear()}</Col>
         </Row>
       </Container>
     </footer>
